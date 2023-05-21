48d97a994c8d6517d3515c120e025e9885a943ea
rufevean <deerajmydearalways@gmail.com>
2023-05-21 15:14:32 +0530

    enhancement - added preloader and animations to it

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,14 +1,39 @@
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './styles/App.css';
+import "./styles/App.css";
+import Preloader from "./components/Preloader";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handlePreloaderClick = () => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    };
+
+    // useEffect(() => {
+    //     // Simulate loading or initialization process
+    //     setTimeout(() => {
+    //         setIsLoading(false);
+    //     }, 2000); // Adjust the delay time (in milliseconds) as needed
+    // }, []);
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            {isLoading ? (
+                <div onClick={handlePreloaderClick}>
+                    <Preloader />
+                </div>
+            ) : (
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </BrowserRouter>
+            )}
+        </div>
     );
 }
 
